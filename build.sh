a540f98216f3ce007bd70e3b2e8df8ed35352e98
Folemaeth <koshelevandr@gmail.com>
2023-01-30 14:16:14 +0300

    Setup

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sudo apt-get install build-essential libtcmalloc-minimal4 git cmake
+sudo ln -s /usr/lib/libtcmalloc_minimal.so.4 /usr/lib/libtcmalloc_minimal.so
+
 rm -rf build
 mkdir build
 cd build
