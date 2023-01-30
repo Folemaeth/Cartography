dbd0510622628969890abadb9bde264ca88a8534
Folemaeth <koshelevandr@gmail.com>
2023-01-30 15:18:27 +0300

    Crow lib

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install build-essential libtcmalloc-minimal4 git cmake
-sudo ln -s /usr/lib/libtcmalloc_minimal.so.4 /usr/lib/libtcmalloc_minimal.so
-
 rm -rf build
 mkdir build
 cd build
 cmake ..
 cmake --build .
+
+$SHELL
