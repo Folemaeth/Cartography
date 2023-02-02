a7f2dc0dd6d4ab7376da36e6fd5eaa537bd0c3d9
Folemaeth <koshelevandr@gmail.com>
2023-02-02 19:33:49 +0300

    fixing include directories

--- init.sh
+++ init.sh
@@ -6,8 +6,7 @@ sudo ln -s /usr/lib/libtcmalloc_minimal.so.4 /usr/lib/libtcmalloc_minimal.so
 
 git clone https://github.com/Microsoft/vcpkg.git ~/vcpkg
 
-mkdir include
-git clone -b master --single-branch https://github.com/CrowCpp/Crow.git ./include
+git clone -b master --single-branch https://github.com/CrowCpp/Crow.git ./include/Crow
 mkdir ./include/Crow/build
 cmake -S ./include/Crow -B ./include/Crow/build -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF
 sudo make install ./include/Crow/build
